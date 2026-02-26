e598c8f885f2e202b511c594230d83b8aac359b1
Tom-De-Jong <tomdejong2009@gmail.com>
2026-02-26 12:46:09 +0100

    Created functionality for numbers to appear based on how much lines are filled with text by the user

--- Koda/src/main.js
+++ Koda/src/main.js
@@ -1,2 +1,30 @@
 const { invoke } = window.__TAURI__.core;
 
+let gutterWrapper = document.querySelector(".gutterWrapper")
+
+
+console.log("script works girly")
+
+let textArea = document.querySelector(".textarea");
+
+textArea.addEventListener("input", (event) => {
+
+  console.log(textArea.value);
+
+  let lines = textArea.value.split(/\n/);
+  console.log(lines);
+
+
+  gutterWrapper.innerHTML = "";
+
+  for (let i = 0; i < lines.length; i++) {
+    let newGutter = document.createElement("div")
+    newGutter.className = "gutter"
+    newGutter.innerHTML = (i + 1) + "."
+
+    gutterWrapper.appendChild(newGutter);
+
+  }
+
+
+})
