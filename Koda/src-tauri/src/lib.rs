6a31082d1fc4464b1412f63efc14a591af796f61
Tom-De-Jong <tomdejong2009@gmail.com>
2026-02-26 15:39:15 +0100

    added functionality to save and open files

--- Koda/src-tauri/src/lib.rs
+++ Koda/src-tauri/src/lib.rs
@@ -3,6 +3,7 @@
 use std::env;
 use std::fs;
 
+
 #[tauri::command(rename_all = "snake_case")]
 fn get_text_file(file_path: String) -> String{
 
@@ -15,13 +16,23 @@ fn get_text_file(file_path: String) -> String{
     contents
 }
 
+#[tauri::command(rename_all = "snake_case")]
+fn save_text_file(file_path: String, text: String){
+
+    println!("In file {file_path}");
+
+    fs::write(&file_path, &text).expect("Should have been able to write the file");
+
+    println!("wrote file successfully");
+}
+
 
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![get_text_file])
+        .invoke_handler(tauri::generate_handler![get_text_file, save_text_file])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- Koda/src/main.js
+++ Koda/src/main.js
@@ -5,24 +5,65 @@ let textareaglobal = document.querySelector(".textarea")
 
 console.log("script works girly")
 
-// function openFile() {
-//   let filepath = "/home/tom/Documents/Koda-text-editor/README.md" //temporary
-//   invoke('get_text_file', { invokeMessage: filepath }).then((message) => console.log(message));;
-// }
 
+//saves the file to the path when topbar save button is clicked
+document.querySelector(".saveFile").addEventListener("click", () => {
+saveFile();
+})
+
+
+//function for saving files
+function saveFile(){
+
+  let filepath = "/home/tom/Documents/Koda-text-editor/README.md" //temporary
+  let text = document.querySelector(".textarea").value;
+
+  invoke('save_text_file', { file_path: filepath, text: text })
+  
+}
+
+// ctrl + s save file
+document.onkeydown = keydown;
+
+function keydown(evt){
+  if (!evt) evt = event;
+  if (evt.ctrlKey && evt.keyCode==83){ //CTRL+S
+    saveFile();
+    alert("File saved"); 
+  }
+
+}
+
+//opens file from filepath
 document.querySelector(".openFile").addEventListener("click", () => {
   console.log("clicked");
 
   let filepath = "/home/tom/Documents/Koda-text-editor/README.md" //temporary
   invoke('get_text_file', { file_path: filepath }).then((message) => {
     console.log(message)
+
+    // check how many lines there are and add them when opening file
+    let lines = message.split(/\n/);
+    console.log(lines);
+
+    gutterWrapper.innerHTML = "";
+
+    for (let i = 0; i < lines.length; i++) {
+      let newGutter = document.createElement("div")
+      newGutter.className = "gutter"
+      newGutter.innerHTML = (i + 1) + "."
+
+      gutterWrapper.appendChild(newGutter);
+
+    }
+
     textareaglobal.innerHTML = message;
   });;
 
 
 })
 
-// this is the script that makes sures lines get added
+// this is the script that makes sures lines get added when you press enter
 
 let textArea = document.querySelector(".textarea");
 
